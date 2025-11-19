868f1b11cd40733eff4afa506b90885d9d3b0dd1
thrdchld <thirdchilddesigner@gmail.com>
2025-11-19 22:39:07 +0700

    Offline Almost 4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,9 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        compilerOptions {
+            jvmTarget.set(JavaVersion.VERSION_11)
+        }
     }
 
     defaultConfig {
@@ -41,42 +43,4 @@ flutter {
 
 dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
-}
-
-android {
-    namespace = "com.example.komitan_kutter"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.komitan_kutter"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-}
-
-flutter {
-    source = "../.."
 }
